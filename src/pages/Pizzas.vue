1210dd0040fcf4463b8545f0f4a5dea988c57ccf
Lucy <lucy.brandao@znc.com.br>
2021-11-11 16:06:28 -0300

    mais ajustes no grid system

--- src/pages/Pizzas.vue
+++ src/pages/Pizzas.vue
@@ -1,15 +1,15 @@
 <template>
-  <q-page class="q-pa-md q-mb-lg">
+  <q-page class="q-pa-md">
     <div class="row justify-center">
-      <div class="col-6 q-mb-lg">
+      <div class="col col-md-10 col-lg-6 q-mb-lg">
         <!-- Começar pedido -->
         <q-card flat class="my-card bg-primary text-white">
           <q-card-section>
             <div class="row items-center">
-              <div class="col-6 text-center">
+              <div class="col col-lg-6 text-center">
                 <div class="text-h5 text-bold text-uppercase">Começar o Pedido</div>
               </div>
-              <div class="col-6">
+              <div class="col col-lg-6">
                 <div class="row justify-around items-center">
                   <q-btn flat class="bg-negative text-white text-bold q-px-xl q-py-sm">Entrega</q-btn>
                   <div class="text-h6 text-bold text-blue-grey-4 text-italic">ou</div>
